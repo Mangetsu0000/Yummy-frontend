96622848a1054f10237009aa2d39b5856d3fc744
Manel Issaoui <Manel.issaoui@myu.universitecentrale.tn>
2022-05-13 22:51:33 +0100

    added login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ɵresetJitOptions } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: '',
+      password: '',
+    });
+  }
+
+  submit(): void {
+    this.authService.signin(this.form.getRawValue()).subscribe((res) => {
+      this.router.navigate(['/profile']);
+      console.log(res);
+    });
+  }
+}
