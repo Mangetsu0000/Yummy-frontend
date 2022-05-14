f3dc4e57ad55cdff822b3756f6670a6f728393e1
Mohamed Fakhreddine Mouelhi <m.mouelhi0603@myu.universitecentrale.tn>
2022-05-14 04:33:35 +0100

    updated nav component

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onLogout() {
+    this.authService.logoutUser();
   }
-
 }
