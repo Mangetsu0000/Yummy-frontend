cd2fa6803fee3f2a986dc05d2874d5ed6de0264c
Mohamed Fakhreddine Mouelhi <m.mouelhi0603@myu.universitecentrale.tn>
2022-05-14 04:34:14 +0100

    updated profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  form!: FormGroup;
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInist(): void {
+    this.userService.getMe().subscribe(
+      (res) =>
+        (this.form = new FormGroup({
+          firstName: res.firstName,
+          lastName: res.lastName,
+          email: res.email,
+        }))
+    );
   }
 
+  ngOnInit(): void {
+    this.userService.getMe().subscribe((res) => {
+      this.form = this.formBuilder.group({
+        firstName: res.firstName,
+        lastName: res.lastName,
+        email: res.email,
+      });
+    });
+  }
 }
