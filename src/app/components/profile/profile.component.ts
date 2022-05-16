2d294e1d1a70b0bf6455f1f491b79bc853236b8c
Mohamed Fakhreddine Mouelhi <94762437+Mangetsu0000@users.noreply.github.com>
2022-05-16 23:25:59 +0100

    Developed Profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -9,9 +11,14 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class ProfileComponent implements OnInit {
   form!: FormGroup;
+  file: any;
+  editProfile: boolean = false;
+  showSubmitRecipe: boolean = false;
   constructor(
     private userService: UserService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    router: Router,
+    private http: HttpClient
   ) {}
 
   ngOnInist(): void {
@@ -33,5 +40,31 @@ export class ProfileComponent implements OnInit {
         email: res.email,
       });
     });
+  }
+  showEdit() {
+    this.editProfile = true;
+    this.showSubmitRecipe = false;
+  }
+  showSumbitRecipe() {
+    this.showSubmitRecipe = true;
+    this.editProfile = false;
+  }
+  submit() {
+    this.editProfile = false;
+  }
+  getFile(event: any) {
+    this.file = event.target.files[0];
+  }
+  uploadFile() {
+    let formData = new FormData();
+    formData.set('file', this.file);
+
+    //call Api
+    this.http
+      .post('http://localhost:3001/upload/uploadFiles', formData)
+      .subscribe((response) => {});
+  }
+  submitRecipe() {
+    this.showSubmitRecipe = false;
   }
 }
